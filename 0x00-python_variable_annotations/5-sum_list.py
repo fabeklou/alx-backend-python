b008a2c8f5ba1d69b5073b02edec13231780b939
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-06 11:49:28 +0000

    feat: type annotate a function using typing.List

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+"""This module contains a sum_list  function which
+takes a List[float] as argument and
+returns their sum as a float.
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a List[float] as argument and
+    returns their sum as a float
+
+    Args:
+        input_list (List[float]): the list of floats
+            to sum
+
+    Returns:
+        float: sum of floating point numbers in the
+            the given list
+    """
+    return sum(input_list)
