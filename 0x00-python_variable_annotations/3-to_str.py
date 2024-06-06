dbff696f682c5d94793ebfbfb1ca480f0e648bd3
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-06 11:30:32 +0000

    feat: type-annotate to_str() function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""This module contains a to_str function that receives
+an argument of type float and returns his string representation
+"""
+
+
+def to_str(number: float) -> str:
+    """receives an argument of type float
+    and returns his string representation
+
+    Args:
+        number (float): the floating point number
+            to be converted into string
+
+    Returns:
+        str: string representation of the given number
+    """
+    return str(number)
