1638b56fcf15773a6969f29887646ccfd3e67d78
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-06 11:07:14 +0000

    feat: type-annotate the add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""This module contains an add function thatcadd two
+floating point values together and retrun the result
+"""
+
+
+def add(a: float, b: float) -> float:
+    """add two floating point values together and
+    retrun the result
+
+    Args:
+        a (float): first value
+        b (float): second value
+
+    Returns:
+        float: result of a + b
+    """
+    return a + b
