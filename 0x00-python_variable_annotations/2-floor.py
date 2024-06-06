20924b9fc0f47b968ff55aaacd01ee5e0c878149
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-06 11:22:12 +0000

    feat: type-annotate the floor() function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""This module contains a floor function
+that taks in a floating point number and
+returns his floor
+"""
+
+
+def floor(number: float) -> int:
+    """taks in a floating point number
+    and returns his floor
+
+    Args:
+        number (float): the floating point number
+            which floor to return
+
+    Returns:
+        int: floor of the given number
+    """
+    return int(number)
