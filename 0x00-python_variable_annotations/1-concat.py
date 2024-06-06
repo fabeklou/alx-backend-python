a4f26e3ee1f9ec22b67463d25a1c6af1ef2d8d7b
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-06 11:15:17 +0000

    feat: type-annotate the concat() function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""This module contains a concat function that
+taks in two strings, concatenate them and
+return the result
+"""
+
+
+def concat(string_a: str, string_b: str) -> str:
+    """taks in two strings, concatenate them
+    and return the result
+
+    Args:
+        string_a (str): first string
+        string_b (str): second string
+
+    Returns:
+        str: result of the concatenation of
+            string_a and string_b
+    """
+    return string_a + string_b
