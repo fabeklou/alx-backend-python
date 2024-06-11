aeb4e9e065c0b274a3a473d1d4f301c7ae1a2cb4
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-11 09:51:08 +0000

    feat: create async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,26 @@
+#!usr/bin/env python3
+
+"""This module contains an async_generator function
+that yields random values
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator, Any
+
+
+async def async_generator() -> AsyncGenerator[float, Any]:
+    """
+    Asynchronous generator that yields random values.
+
+    This generator yields random floating-point values between 0 and 10.
+    It uses asyncio.sleep to introduce a delay of 1 second between each yield.
+
+    Yields:
+        float: A random floating-point value between 0 and 10.
+
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        random_value: float = random.uniform(0, 10)
+        yield random_value
