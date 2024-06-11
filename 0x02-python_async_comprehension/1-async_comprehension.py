7a8b16cdf633ddbc36bbd01c3e8eb4aa8304a0c8
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-11 09:52:33 +0000

    feat: use async list comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3
+
+"""
+This module contains a function for generating a list
+of random floats using async comprehension.
+"""
+
+from typing import Any, List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Generate a list of random floats using async comprehension.
+
+    Returns:
+        A list of random floats.
+    """
+    random_floats: List[float] = [flt async for flt in async_generator()]
+    return random_floats
