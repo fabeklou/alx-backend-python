08dd6e182286f3291e136a093800617e854e1a36
Fabrice Eklou <fab.eklou@gmail.com>
2024-06-11 13:45:29 +0000

    fix: rm random.uniform and use random.random

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,19 +6,21 @@ that yields random values
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields random values.
 
-    This generator yields random floating-point values between 0 and 10.
-    It uses asyncio.sleep to introduce a delay of 1 second between each yield.
+    This generator yields random floating-point values
+    between 0 and 10.
+    It uses asyncio.sleep to introduce a delay of 1 second
+    between each yield.
 
     Yields:
         float: A random floating-point value between 0 and 10.
-
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
